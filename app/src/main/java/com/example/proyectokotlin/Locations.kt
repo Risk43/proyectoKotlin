3e135d006db4b780f01790500337846645cb612c
Risk43 <ricardo_esap@outlook.es>
2023-06-15 15:56:43 -0600

    Cambio menor

--- app/src/main/java/com/example/proyectokotlin/Locations.kt
+++ app/src/main/java/com/example/proyectokotlin/Locations.kt
@@ -1,5 +1,7 @@
 package com.example.proyectokotlin
 
-data class Locations(val img:String="DEFAULT_IMG", val info:String="DEFAULT_DESC", val name: String="DEFAULT_NAME")
+data class Locations(val img:String="DEFAULT_IMG",
+                     val info:String="DEFAULT_DESC",
+                     val name: String="DEFAULT_NAME")
 
 
