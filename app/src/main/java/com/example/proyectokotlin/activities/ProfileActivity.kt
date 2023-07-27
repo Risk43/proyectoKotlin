0182d60e66cf0221b92c713baf4e1b494038671d
Risk43 <ricardo_esap@outlook.es>
2023-07-26 19:47:49 -0600

    Profile image upload

--- app/src/main/java/com/example/proyectokotlin/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/proyectokotlin/activities/ProfileActivity.kt
@@ -2,16 +2,28 @@ package com.example.proyectokotlin.activities
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.proyectokotlin.R
+import com.google.common.base.MoreObjects.ToStringHelper
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class ProfileActivity : AppCompatActivity() {
 
+    private lateinit var session: String
 
     private lateinit var profile: TextView
     private lateinit var profileName: TextView
@@ -20,20 +32,26 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var ivBack: ImageView
     private lateinit var ivSingOut: ImageView
+    private lateinit var ivProfilePhoto: ImageView
+
+    private lateinit var btUpload: Button
+    private lateinit var btEditPhoto: ImageButton
+
+    private lateinit var progressBar: ProgressBar
+
+    private lateinit var storageReference: StorageReference
+    private lateinit var rute_storage: String
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        initVars()
+
         val bundle: Bundle? = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
-
-
-        profile = findViewById(R.id.tvProfile)
-        profileName = findViewById(R.id.tvProfileName)
-        profileLName = findViewById(R.id.tvProfileLName)
-        profileEmail = findViewById(R.id.tvProfileEmail)
 
         val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
         prefs.putString("email",email)
@@ -41,19 +59,33 @@ class ProfileActivity : AppCompatActivity() {
         prefs.apply()
 
         val prefsRec = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
-        val session = prefsRec.getString("email", null)
+        session = prefsRec.getString("email", null).toString()
 
 
-        modules(session!!, ProviderType.EMAIL_PASSWORD)
+        initComponents(session!!, ProviderType.EMAIL_PASSWORD)
 
     }
 
-    fun modules(session: String, provider: ProviderType){
+    private fun initVars(){
+        profile = findViewById(R.id.tvProfile)
+        profileName = findViewById(R.id.tvProfileName)
+        profileLName = findViewById(R.id.tvProfileLName)
+        profileEmail = findViewById(R.id.tvProfileEmail)
+        ivProfilePhoto = findViewById(R.id.ivProfilePhoto)
+        btUpload = findViewById(R.id.btUpload)
+        btEditPhoto = findViewById(R.id.btEditPhoto)
+        progressBar = findViewById(R.id.progresBar)
+        storageReference = FirebaseStorage.getInstance().reference
+
+        btUpload.visibility = View.INVISIBLE
+    }
+    private fun initComponents(session: String, provider: ProviderType){
         setData(session)
         goMain(session, provider)
         singOut()
+        uploadPhoto()
+        updatePhoto(session)
     }
-
 
     private fun singOut(){
         ivSingOut = findViewById(R.id.ivSingOut)
@@ -78,12 +110,74 @@ class ProfileActivity : AppCompatActivity() {
                         val name = document.getString("name")
                         val lName = document.getString("lastName")
                         val email = document.getString("email")
+                        val photo = document.getString("photo")
+
 
                         profile.text = name
                         profileName.text = name
                         profileLName.text = lName
                         profileEmail.text = email
+                        Glide.with(this).load(photo).into(ivProfilePhoto)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun uploadPhoto(){
+        btEditPhoto.setOnClickListener {
+            resultLauncher.launch("image/*")
+            btUpload.visibility = View.VISIBLE
+        }
+        btUpload.setOnClickListener {
+            selectPhoto()
+            btUpload.visibility = View.INVISIBLE
+        }
+    }
+
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()){
+        imageUri = it
+        ivProfilePhoto.setBackgroundResource(R.color.transparent)
+        ivProfilePhoto.setImageURI(imageUri)
+    }
+
+
+    private fun selectPhoto(){
+        progressBar.visibility = View.VISIBLE
+        rute_storage = "users/" + session
+        val reference = storageReference.child(rute_storage)
+        reference.putFile(imageUri).addOnCompleteListener { task->
+            if (task.isSuccessful){
+                reference.downloadUrl.addOnSuccessListener { uri->
+                    val downloadUri = uri.toString()
+                    val map = hashMapOf<String, Any>()
+                    map.put("photo",downloadUri)
+
+                       FirebaseFirestore.getInstance().collection("users").document(session).update(map).addOnCompleteListener{ firestoreTask->
+                           if (firestoreTask.isSuccessful){
+                               Toast.makeText(this,"Foto actualizada", Toast.LENGTH_SHORT).show()
+                           }else{
+                               Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                           }
+                           progressBar.visibility = View.GONE
+                       }
+                }
+            }else{
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updatePhoto(session: String){
+        if (session != null){
+            FirebaseFirestore.getInstance().collection("users").get().addOnSuccessListener { result ->
+                for (document in result){
+                    if (document.getString("email") == session){
+                        val photo = document.getString("photo")
+
+                        Glide.with(this).load(photo).into(ivProfilePhoto)
                     }
                 }
             }
